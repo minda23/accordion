91bdee27cd8174d953132db2560a04ff5150f1d8
minda23 <luki.mindas@gmail.com>
2025-09-17 21:45:34 +0200

    fix bug

--- js/script.js
+++ js/script.js
@@ -3,16 +3,7 @@
 const accordionItems = document.getElementsByClassName("accordion__item");
 const accordionText = document.getElementsByClassName("accordion__text");
 const iconPlus = document.getElementsByClassName("accordion__open")
-const iconMinus = document.getElementsByClassName("accordion__close")
-
-console.log(accordionItems)
-
-// chceme vlastne prejsť cet accordion item a potom na kliknutie textu chceme vlastne robiť toggle
-// chceme pridať interaktivu accordion items preto robime forEach
-// použiť overflow a height 
-// 1. is-open ma niejaku velku vysku (lebo je otvorene), a inac ten komponent / element ma malu vysku
-// (popripade pouzit !important)
-// ked to spravim treba to pushnut a commitovať a vymazať komenty
+const iconMinus = document.getElementsByClassName("accordion__close");
 
 document.addEventListener("DOMContentLoaded", () => {
   for (let i = 0; i < accordionItems.length; i++) {
