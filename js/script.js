7b500a3a48fd16b9a971bf249c9ba9ba9418201d
minda23 <luki.mindas@gmail.com>
2025-09-17 21:45:15 +0200

    make vanilla js functional with accordion

--- js/script.js
+++ js/script.js
@@ -1,16 +1,39 @@
 
 
-const accordionItems = document.getElementsByClassName("accordion-item");
-const accordiontext = document.getElementsByClassName("accordion-text");
+const accordionItems = document.getElementsByClassName("accordion__item");
+const accordionText = document.getElementsByClassName("accordion__text");
+const iconPlus = document.getElementsByClassName("accordion__open")
+const iconMinus = document.getElementsByClassName("accordion__close")
+
+console.log(accordionItems)
 
 // chceme vlastne prejsť cet accordion item a potom na kliknutie textu chceme vlastne robiť toggle
+// chceme pridať interaktivu accordion items preto robime forEach
+// použiť overflow a height 
+// 1. is-open ma niejaku velku vysku (lebo je otvorene), a inac ten komponent / element ma malu vysku
+// (popripade pouzit !important)
+// ked to spravim treba to pushnut a commitovať a vymazať komenty
 
-accordionItems.forEach(item => {
-      accordionItems.addEventListener('click', e => {
-           if (accordiontext.style.display === "none") {
-    accordiontext.style.display = "block";
-  } else {
-    accordiontext.style.display = "none";
+document.addEventListener("DOMContentLoaded", () => {
+  for (let i = 0; i < accordionItems.length; i++) {
+    const item = accordionItems[i];
+    const text = accordionText[i];
+    const plus = iconPlus[i];
+    const minus = iconMinus[i];
+
+
+    item.addEventListener("click", () => {
+      text.classList.toggle("is-open");
+
+      if (text.classList.contains("is-open")) {
+        plus.style.display = "none";
+        minus.style.display = "block";
+      } else {
+        plus.style.display = "block";
+        minus.style.display = "none";
+      }
+    });
   }
-  });
 });
+
+
